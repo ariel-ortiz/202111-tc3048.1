8ac4d8ad1b7d12cf1b3783cdbcb7d1c0da97fdf8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-04-21 15:36:36 -0500

    Añade clase Node para la construcción del AST

--- Simple Expression Language/simple_expression.cs
+++ Simple Expression Language/simple_expression.cs
@@ -23,6 +23,7 @@ Gramática LL(1) equivalente:
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 
 public enum TokenCategory {
@@ -162,6 +163,51 @@ public class Parser {
         }
     }
 
+}
+
+public class Node: IEnumerable<Node> {
+
+    IList<Node> children = new List<Node>();
+
+    public Node this[int index] {
+        get {
+            return children[index];
+        }
+    }
+
+    public Token AnchorToken { get; set; }
+
+    public void Add(Node node) {
+        children.Add(node);
+    }
+
+    public IEnumerator<Node> GetEnumerator() {
+        return children.GetEnumerator();
+    }
+
+    System.Collections.IEnumerator
+    System.Collections.IEnumerable.GetEnumerator() {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString() {
+        return $"{GetType().Name} {AnchorToken}";
+    }
+
+    public string ToStringTree() {
+        var sb = new StringBuilder();
+        TreeTraversal(this, "", sb);
+        return sb.ToString();
+    }
+
+    static void TreeTraversal(Node node, string indent, StringBuilder sb) {
+        sb.Append(indent);
+        sb.Append(node);
+        sb.Append('\n');
+        foreach (var child in node.children) {
+            TreeTraversal(child, indent + "  ", sb);
+        }
+    }
 }
 
 public class Driver {
