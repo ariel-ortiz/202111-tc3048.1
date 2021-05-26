2a994fd70ce4dfd8f3c7bda048a89b3c399a55c8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-05-26 18:49:14 -0500

    Agrega script de ejecución

---
+++ Simple Expression Language/execute.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Wat execution script.
+# Copyright (C) 2021 Ariel Ortiz, ITESM CEM
+
+from wasmer import engine, Module, wat2wasm, Store, Instance
+from wasmer import ImportObject, Function
+from wasmer_compiler_cranelift import Compiler
+
+def make_import_object(store):
+
+    def pow(base: int, expo: int) -> int:
+        return base ** expo
+
+    import_object = ImportObject()
+    import_object.register(
+        "math",
+        {
+            "pow": Function(store, pow)
+        }
+    )
+    return import_object
+
+def create_instance(file_name):
+    store = Store(engine.JIT(Compiler))
+    with open(file_name) as wat_file:
+        wat_source_code = wat_file.read()
+    module = Module(store, wat2wasm(wat_source_code))
+    return Instance(module, make_import_object(store))
+
+def main():
+    instance = create_instance('output.wat')
+    print(instance.exports.inicio.start())
+
+main()
